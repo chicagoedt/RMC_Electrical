dd66792e9fc1435ee9dfdaf0a342446c88e9003e
Basheer Subei <basheersubei@gmail.com>
2015-05-15 16:01:48 -0500

    adds define for UNO pins

--- Sigmux2.0/cc3000/cc3000.ino
+++ Sigmux2.0/cc3000/cc3000.ino
@@ -4,12 +4,23 @@
 #include <string.h>
 #include "utility/debug.h"
 
+#define UNO
+
 #define	LOG_DATA
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   7      // MUST be an interrupt pin!
-// These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  6
-#define ADAFRUIT_CC3000_CS    17     //  http://forum.arduino.cc/index.php?topic=241369.0
+
+// These are the interrupt and control pins for the UNO
+#ifdef UNO
+  #define ADAFRUIT_CC3000_IRQ   3      // MUST be an interrupt pin!
+  // These can be any two pins
+  #define ADAFRUIT_CC3000_VBAT  5
+  #define ADAFRUIT_CC3000_CS    10     //  http://forum.arduino.cc/index.php?topic=241369.0
+// These are the interrupt and control pins for the ATmega32u4 (same as Leonardo)
+#else
+  #define ADAFRUIT_CC3000_IRQ   7      // MUST be an interrupt pin!
+  // These can be any two pins
+  #define ADAFRUIT_CC3000_VBAT  6
+  #define ADAFRUIT_CC3000_CS    17     //  http://forum.arduino.cc/index.php?topic=241369.0
+#endif
 
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
