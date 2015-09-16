3a2b9cee3f8e91e65957c74fa9671d9ccea75203
Ammar Subei <ammarsubei@gmail.com>
2015-09-16 17:00:46 -0500

    Early sigmux board new design.

---
+++ Sigmux2.0/layout/sigmuxBoard.cmd
@@ -0,0 +1,131 @@
+# Pin name action command file
+
+# Start of element D105
+ChangePinName(D105, 2, 2)
+ChangePinName(D105, 1, 1)
+
+# Start of element D104
+ChangePinName(D104, 2, 2)
+ChangePinName(D104, 1, 1)
+
+# Start of element D103
+ChangePinName(D103, 2, 2)
+ChangePinName(D103, 1, 1)
+
+# Start of element D102
+ChangePinName(D102, 2, 2)
+ChangePinName(D102, 1, 1)
+
+# Start of element D101
+ChangePinName(D101, 2, 2)
+ChangePinName(D101, 1, 1)
+
+# Start of element CONN101
+ChangePinName(CONN101, 1, 1)
+ChangePinName(CONN101, 2, 2)
+
+# Start of element C105
+ChangePinName(C105, 2, 2)
+ChangePinName(C105, 1, 1)
+
+# Start of element C104
+ChangePinName(C104, 2, 2)
+ChangePinName(C104, 1, 1)
+
+# Start of element C103
+ChangePinName(C103, 2, 2)
+ChangePinName(C103, 1, 1)
+
+# Start of element C102
+ChangePinName(C102, 2, 2)
+ChangePinName(C102, 1, 1)
+
+# Start of element C101
+ChangePinName(C101, 2, 2)
+ChangePinName(C101, 1, 1)
+
+# Start of element J101
+ChangePinName(J101, 9, 9)
+ChangePinName(J101, 7, 7)
+ChangePinName(J101, 5, 5)
+ChangePinName(J101, 3, 3)
+ChangePinName(J101, 1, 1)
+ChangePinName(J101, 8, 8)
+ChangePinName(J101, 6, 6)
+ChangePinName(J101, 4, 4)
+ChangePinName(J101, 2, 2)
+
+# Start of element U110
+ChangePinName(U110, 11, INVALID)
+ChangePinName(U110, 12, T2in)
+ChangePinName(U110, 13, T1in)
+ChangePinName(U110, 14, FORCEON)
+ChangePinName(U110, 15, R1out)
+ChangePinName(U110, 16, R1in)
+ChangePinName(U110, 17, T1out)
+ChangePinName(U110, 18, GND)
+ChangePinName(U110, 19, Vcc)
+ChangePinName(U110, 20, FORCEOFF)
+ChangePinName(U110, 10, R2out)
+ChangePinName(U110, 9, R2in)
+ChangePinName(U110, 8, T2out)
+ChangePinName(U110, 7, V-)
+ChangePinName(U110, 6, C2-)
+ChangePinName(U110, 5, C2+)
+ChangePinName(U110, 4, C1-)
+ChangePinName(U110, 3, V+)
+ChangePinName(U110, 2, C1+)
+ChangePinName(U110, 1, EN)
+
+# Start of element U111
+ChangePinName(U111, 51, 3V3)
+ChangePinName(U111, 50, GND)
+ChangePinName(U111, 49, A13)
+ChangePinName(U111, 48, A12)
+ChangePinName(U111, 47, D33)
+ChangePinName(U111, 46, D32)
+ChangePinName(U111, 45, D31/A20)
+ChangePinName(U111, 44, D30/A19/SDA1)
+ChangePinName(U111, 43, D29/A18/SCL1)
+ChangePinName(U111, 42, D28/A17)
+ChangePinName(U111, 41, D27/A16)
+ChangePinName(U111, 40, D26/A15)
+ChangePinName(U111, 39, D25)
+ChangePinName(U111, 38, D24)
+ChangePinName(U111, 37, VUSB)
+ChangePinName(U111, 36, AREF)
+ChangePinName(U111, 35, A10)
+ChangePinName(U111, 34, A11)
+ChangePinName(U111, 33, VIN)
+ChangePinName(U111, 32, AGND)
+ChangePinName(U111, 31, 3V3)
+ChangePinName(U111, 30, D23/A9)
+ChangePinName(U111, 29, D22/A8)
+ChangePinName(U111, 28, D21/A7)
+ChangePinName(U111, 27, D20/A6)
+ChangePinName(U111, 26, D19/A5/SCL0)
+ChangePinName(U111, 25, D18/A4/SDA0)
+ChangePinName(U111, 24, D17/A3)
+ChangePinName(U111, 23, D16/A2)
+ChangePinName(U111, 22, D14/A1)
+ChangePinName(U111, 21, D14/A0)
+ChangePinName(U111, 20, "D13(LED)/SCK")
+ChangePinName(U111, 19, DAC/A14)
+ChangePinName(U111, 18, PROG)
+ChangePinName(U111, 17, GND)
+ChangePinName(U111, 16, 3V3)
+ChangePinName(U111, 15, VBAT)
+ChangePinName(U111, 14, D12/DIN)
+ChangePinName(U111, 13, D11/DOUT)
+ChangePinName(U111, 12, D10/TX2/CS)
+ChangePinName(U111, 11, D9/RX2)
+ChangePinName(U111, 10, D8/TX3)
+ChangePinName(U111, 9, D7/RX3)
+ChangePinName(U111, 8, D6)
+ChangePinName(U111, 7, D4)
+ChangePinName(U111, 6, D4)
+ChangePinName(U111, 5, D3)
+ChangePinName(U111, 4, D2)
+ChangePinName(U111, 3, D1/TX1)
+ChangePinName(U111, 2, D0/RX1)
+ChangePinName(U111, 1, GND)
